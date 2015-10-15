8801abf133d2c56e44707187009e67be24bf838a
Alyssa <alyssa@Alyssas-MacBook-Pro.local>
2015-10-14 22:03:37 -0400

    ready w/o remote

--- vidDisplay_wButton_multiPreset/vidDisplay_wButton_multiPreset.ino
+++ vidDisplay_wButton_multiPreset/vidDisplay_wButton_multiPreset.ino
@@ -34,11 +34,12 @@ OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, WS2811_800kHz);
 File videofile;
 
 // *************************added from multiButtonPress *************************
-//for tC - new preset: 19 and 22, reset: 23 and 1
+
 const int  buttonPresetPin = 19;    // the pin that the pushbutton is attached to. other side to GND
-const int  buttonPresetPinPwr = 22;
 const int  buttonResetPin = 23;    // the pin that the pushbutton is attached to. other side to GND
-const int  buttonResetPinPwr = 1;
+//const int  remotePresetPin = 23;
+//const int  remotePresetPinBack = 1;
+//const int  remoteResetPin = 0;
 
 // button counter for Preset 
 int buttonPushCounter = 0;   // counter for the number of button presses
@@ -46,18 +47,35 @@ int buttonState = 1;         // current state of the button
 int lastButtonState = 0;     // previous state of the button
 
 // button counter for Reset 
-int buttonPushCounterReset = 0;   // counter for the number of button presses
-int buttonStateReset = 1;         // current state of the button
-int lastButtonStateReset = 0;     // previous state of the button
+int buttonPushCounterReset = 0;
+int buttonStateReset = 1;
+int lastButtonStateReset = 0;
+
+//// remote counter for Preset 
+//int remotePushCounter = 0;   
+//int remoteState = 1;         
+//int lastRemoteState = 0;        
+//
+//// remote counter for Reset 
+//int remotePushCounterReset = 0;   
+//int remoteStateReset = 1;         
+//int lastRemoteStateReset = 0;
+
+//********add after above buttons/remote work *****************************
+// remote counter for back one Preset 
+//int remotePushCounterBack = 0;   
+//int remoteStateBack = 1;         
+//int lastRemoteStateBack = 0;  
 
 //setup for filename 
 //will eventually replace below with counting num of presets from SD card and storing names
-int numPresets = 35;
+int numPresets = 44;
 //change for TA, TB, or TC
-char *filenames[] = { "T1.BIN","T2.BIN","T3.BIN","T4.BIN","T5.BIN","T6.BIN","T7.BIN","T8.BIN","T9.BIN","T10.BIN",
+char *filenames[] = { "T.bin","T1.bin","T2.BIN","T3.BIN","T4.BIN","T5.BIN","T6.BIN","T7.BIN","T8.BIN","T9.BIN","T10.BIN",
 "T11.BIN","T12.BIN","T13.BIN","T14.BIN","T15.BIN","T16.BIN","T17.BIN","T18.BIN","T19.BIN","T20.BIN",
 "T21.BIN","T22.BIN","T23.BIN","T24.BIN","T25.BIN","T26.BIN","T27.BIN","T28.BIN","T29.BIN","T30.BIN",
-"T31.BIN","T32.BIN","T33.BIN","T34.BIN","T35.BIN"};
+"T31.BIN","T32.BIN","T33.BIN","T34.BIN","T35.BIN","T26.BIN","T27.BIN","T28.BIN","T29.BIN","T40.BIN",
+"T41.BIN","T42.BIN","T43.BIN" };
 
 //char *filenames[] = { "T1A.bin","T1B.bin","T1C.bin","T2A.bin","T2B.bin","T4A.bin","T4B.bin","T5A.bin","T5B.bin","T5C.bin",
 //"T7A.bin","T7B.bin","T8A.bin","T8B.bin","T8C.bin","T8D.bin","T10A.bin","T10B.bin","T10C.bin","T11A.bin","T11B.bin",
@@ -75,10 +93,13 @@ File root;
 
 void setup() {
   pinMode(buttonPresetPin, INPUT_PULLUP);
-  pinMode(buttonPresetPinPwr, OUTPUT);
-  
   pinMode(buttonResetPin, INPUT_PULLUP);
-  pinMode(buttonResetPinPwr, OUTPUT);
+//  pinMode(remotePresetPin, INPUT_PULLUP);
+//  pinMode(remotePresetPinBack, INPUT_PULLUP);
+//  pinMode(remoteResetPin, INPUT_PULLUP);
+  
+  //********add after above buttons/remote work *****************************
+
 //  pinMode(17, INPUT_PULLUP);
 //  pinMode(19, INPUT_PULLUP);
 //  pinMode(22, INPUT_PULLUP);
@@ -87,9 +108,7 @@ void setup() {
 //  pinMode(0, INPUT_PULLUP);
   
 
-  //ADDED - we use a pullup resistor so opp side must be ground
-  digitalWrite(buttonPresetPinPwr, LOW);
-  digitalWrite(buttonResetPinPwr, LOW);
+
 
   Serial.begin(9600);
 //  while (!Serial){
@@ -106,6 +125,7 @@ void setup() {
   Serial.println("File opened");
   playing = true;
   elapsedSinceLastFrame = 0;
+  
 }
 
 // read from the SD card, true=ok, false=unable to read
@@ -248,6 +268,75 @@ void loop() {
     delay(50);
   }
   lastButtonStateReset = buttonStateReset;
+  //********************************************************************************
+  // *************************added from multiButtonPress FOR PRESET Remote *******
+//  remoteState = digitalRead(remotePresetPin);
+//  // compare the buttonState to its previous state
+//  if (remoteState != lastRemoteState) {
+//    // ADDED - switched to be low, which would indicate pushed button
+//    if (remoteState == LOW) {
+//      remotePushCounter++;
+//      incrementVid = true;
+//      error("new preset");
+//      if (curFileNum == (numPresets-1) ) {
+//          curFileNum = 0;
+//      } else {
+//          curFileNum++;
+//      }
+//      //Serial.println("on");
+//      Serial.print("number of remote forward pushes:  ");
+//      Serial.println(remotePushCounter); 
+//    } else {
+//      //Serial.println("off");
+//    }
+//    delay(50);
+//  }
+//  lastRemoteState = remoteState;
+//  //********************************************************************************
+//  
+//  // *************************added from multiButtonPress FOR RESET Remote *******
+//  remoteStateReset = digitalRead(remoteResetPin);
+//  // compare the buttonState to its previous state
+//  if (remoteStateReset != lastRemoteStateReset) {
+//    // ADDED - switched to be low, which would indicate pushed button
+//    if (remoteStateReset == LOW) {
+//      remotePushCounterReset++;
+//      incrementVid = true;
+//      error("reset");
+//      curFileNum = 0;
+//      //Serial.println("on");
+//      Serial.print("number of remote reset pushes:  ");
+//      Serial.println(remotePushCounterReset); 
+//    } else {
+//      //Serial.println("off");
+//    }
+//    delay(50);
+//  }
+//  lastRemoteStateReset = remoteStateReset;
+//  //********************************************************************************
+//  // *************************added from multiButtonPress FOR PRESET Back Remote *******
+//  remoteStateBack = digitalRead(remotePresetPinBack);
+//  // compare the buttonState to its previous state
+//  if (remoteStateBack != lastRemoteStateBack) {
+//    // ADDED - switched to be low, which would indicate pushed button
+//    if (remoteStateBack == LOW) {
+//      remotePushCounter++;
+//      incrementVid = true;
+//      error("back one preset");
+//      if (curFileNum == 0 ) {
+//          curFileNum = (numPresets-1);
+//      } else {
+//          curFileNum = curFileNum - 1;
+//      }
+//      //Serial.println("on");
+//      Serial.print("number of back remote pushes:  ");
+//      Serial.println(remotePushCounterBack); 
+//    } else {
+//      //Serial.println("off");
+//    }
+//    delay(50);
+//  }
+//  lastRemoteStateBack = remoteStateBack;
   //********************************************************************************
 }
 
